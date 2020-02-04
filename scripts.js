0e1f542bc69ccdc897b0448b57fe1ee4e0c8532a
Chester Moon <cmoon3740@gmail.com>
2020-02-03 19:26:46 -0800

    setup display based on the three ghosts of christmas

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
+//Retrieve current date and diplay to page.
 $('#currentDay').text(moment().format('dddd, MMMM Do'))
+
+function setPlanner() {
+    $("textarea").each(function() {
+        if ($(this).data("time") < parseInt(moment().format('HH'))) {
+            $(this).addClass('past')
+        }
+        if ($(this).data("time") === parseInt(moment().format('HH'))) {
+            $(this).addClass('present')
+        }
+        if ($(this).data("time") > parseInt(moment().format('HH'))) {
+            $(this).addClass('future')
+        }
+    })
+
+
+
+}
+
+setPlanner()
